cb5ae29b2aaa45f916ce5f918e8f72cafd5bc1b6
Maksym Dmytryshyn <dmytryshyn_max@ukr.net>
2021-04-24 20:17:19 +0300

    Fix code space

--- src/App.js
+++ src/App.js
@@ -5,29 +5,27 @@ const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anaki
 
 const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
 
-function App() {
-  return (
+const author = {
+  name: "Anakin Skywalker",
+  photo: ANAKIN_IMAGE,
+  nickname: "@dart_vader"
+};
+
+const App = () => (
     <div className="night-mode">
-      <Post author = {{
-            name: "Anakin Skywalker",
-            photo: ANAKIN_IMAGE,
-            nickname: "@dart_vader"
-         }}
-         content="WTF? Who is Ray? Why she is Skywalker? Luke...?"
-         image={RAY_IMAGE}
-         date={"26 февр."}
+      <Post 
+        author = {author}
+        content="WTF? Who is Ray? Why she is Skywalker? Luke...?"
+        image={RAY_IMAGE}
+        date={"26 февр."}
       />
-      <Post author = {{
-            name: "Anakin Skywalker",
-            photo: ANAKIN_IMAGE,
-            nickname: "@dart_vader"
-         }}
-         content="WTF? Who is Ray? Why she is Skywalker? Luke...?"
-         image={RAY_IMAGE}
-         date={"26 февр."}
+      <Post 
+        author = {author}
+        content="WTF? Who is Ray? Why she is Skywalker? Luke...?"
+        image={RAY_IMAGE}
+        date={"26 февр."}
       />
     </div>
   );
-}
 
 export default App;
